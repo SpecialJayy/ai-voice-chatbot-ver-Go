c63f2c5908f6ccdd2cb570a66d050b9faacb70c1
Jan Ganczarczyk <jkgplay2005@gmail.com>
2024-04-29 11:24:22 +0200

    added piper wrapper for go

--- go.mod
+++ go.mod
@@ -3,13 +3,20 @@ module golan
 go 1.22.2
 
 require (
+	github.com/JKGplay/piper-voice-darkman v0.0.0-20240429083950-0b16bff88f7a // indirect
+	github.com/adrg/xdg v0.4.0 // indirect
 	github.com/agext/levenshtein v1.2.3 // indirect
+	github.com/amitybell/piper v0.0.0-20240130090909-b170e42b1e09 // indirect
+	github.com/amitybell/piper-asset v0.0.0-20231030194325-d36a29e3b1fd // indirect
+	github.com/amitybell/piper-bin-linux v0.0.0-20231118093037-92b3de178ad8 // indirect
+	github.com/amitybell/piper-bin-windows v0.0.0-20231118093113-cc2cef2f6b74 // indirect
 	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
 	github.com/faiface/beep v1.1.0 // indirect
 	github.com/gopxl/beep v1.4.1 // indirect
 	github.com/hajimehoshi/go-mp3 v0.3.4 // indirect
 	github.com/hajimehoshi/oto v1.0.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/klauspost/compress v1.17.3 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/sashabaranov/go-openai v1.22.0 // indirect
 	github.com/timshannon/go-openal v0.0.0-20170220204755-e0ed4c67cffe // indirect
--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	darkman "github.com/JKGplay/piper-voice-darkman"
+	"github.com/amitybell/piper"
 	"github.com/faiface/beep"
 	"github.com/faiface/beep/speaker"
 	"github.com/faiface/beep/wav"
 	"github.com/sashabaranov/go-openai"
-	"io"
 	"log"
 	"os"
 	"os/exec"
@@ -102,49 +104,21 @@ func sendQueryToChatGpt(query string) (string, error) {
 	return resp.Choices[0].Message.Content, nil
 }
 
-func convertTextToAudioAndSaveMp3ToLocation(text string, location string) error {
-	resp, err := client.CreateSpeech(
-		context.Background(),
-		openai.CreateSpeechRequest{
-			Model: openai.TTSModel1,
-			Input: text,
-			Voice: openai.VoiceEcho,
-		},
-	)
+func piperAndPlayResponse(text string) error {
+	tts, err := piper.New("", darkman.Asset)
 	if err != nil {
 		return err
 	}
-	f, err := os.Create(location + "/response.mp3")
+	wavBytes, err := tts.Synthesize(text)
 	if err != nil {
 		return err
 	}
-	defer f.Close()
-	_, err = io.Copy(f, resp.ReadCloser)
+	r := bytes.NewReader(wavBytes)
+	streamer, format, err := wav.Decode(r)
 	if err != nil {
 		return err
 	}
-	return nil
-}
-
-func piper(text string) error {
-	cmd, err := exec.Command("cmd", "/C", "echo "+text+" | .\\piper\\piper.exe --model .\\piper\\pl_PL-darkman-medium.onnx --output_file audio/response.wav").Output()
-	fmt.Println(string(cmd))
-	return err
-}
-
-func playMp3Response() error {
-	f, err := os.Open("audio/response.wav")
-	if err != nil {
-		return err
-	}
-	streamer, format, err := wav.Decode(f)
-	if err != nil {
-		return err
-	}
-	defer streamer.Close()
-
 	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
-
 	done := make(chan bool)
 	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
 		done <- true
@@ -174,15 +148,9 @@ func main() {
 		return
 	}
 	fmt.Println(answer)
-	//err = convertTextToAudioAndSaveMp3ToLocation(answer, "audio")
-	err = piper(answer)
+	err = piperAndPlayResponse(answer)
 	if err != nil {
 		fmt.Println(err)
-		return
-	}
-	err = playMp3Response()
-	if err != nil {
-		fmt.Println("Beep error", err)
 		return
 	}
 }
